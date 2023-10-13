8d324d97477acaf8f49ce72c34c4b77312f88541
chominji0716 <145107983+chominji0716@users.noreply.github.com>
2023-10-13 16:04:23 +0900

    Update hw6(1).cpp

--- hw6(1).cpp
+++ hw6(1).cpp
@@ -46,3 +46,45 @@ float getLength(int eumpyo) {
     else return -1;
     return T;
 }
+
+int main() {
+    ifstream xx("my.wav", ios::binary | ios::in);
+    if (!xx) return 999;  // if the file is not found
+    char header[44];
+    // Sprint 1
+    xx.read(header, 44);
+    xx.close();
+    int* sampleRate = (int*)(header + 24);
+    cout << "   sampleRate = " << *sampleRate << endl;
+    short* n;   n = (short*)(header + 22);  cout << *n << endl; // numChannels
+    int* fs;   fs = (int*)(header + 24);  cout << *fs << endl; // sampleRate
+    int* b;   b = (int*)(header + 28);  cout << *b << endl;  //  byteRate
+    *n = 2;  // streo
+    int N = fs[0] * 10; // number of samples for 1 second
+    short* data;
+    data = new short[N]; // allocate memory for 1 seconds mono
+    const float pi = 3.141592;
+    float dt = 1. / fs[0];
+
+    ofstream yy("my3.wav", ios::binary | ios::out);
+    if (!yy) return 666;  // 만일 파일이 열리지 않으면 프로그램을 끝낸다. 
+    yy.write(header, 100 * sizeof(char)); // 헤더를 쓴다. 
+    ifstream zz("note.txt");
+    int f, a, T, tt, ss;
+    float t = 0;
+    string hh;
+    int k;
+    zz >> k;
+    for (int i = 0; i < k; i++) {
+        zz >> tt >> hh >> ss;
+        f = getfrequency(hh);
+        a = getAmplitude(ss);
+        T = getLength(tt);
+        for (int j = 0; j < fs[0]; j++, t += dt) {
+            data[j] = (short)(a * sin(2.0 * pi * f * t));   // a 진폭, f 주파수, N 샘플수(1초는 fs[0] samples, 44100 또는 8000)
+        }
+        yy.write((char*)data, fs[0] * sizeof(short));    // 1초 분량 쓴다.
+    }
+    yy.close();
+    return 1;
+}
