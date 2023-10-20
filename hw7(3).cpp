a64e105248557729a6b16c97db36357e3f9330ff
chominji0716 <145107983+chominji0716@users.noreply.github.com>
2023-10-20 15:35:11 +0900

    Create hw7(3).cpp

---
+++ hw7(3).cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void savemultiple(int *arr) {
+    for (int i = 1000; i <= 5000; i += 1000) {
+        *arr = i * 5;
+        arr++;
+    }
+}
+
+void savesquare(float* arr) {
+    for (float i = -0.5; i < 1.0; i += 0.5) {
+        *arr = i * i;
+        arr++;
+    }
+}
+
+int main() {
+    ofstream file("mybin3.dat", ios::binary);
+    if (!file.is_open()) {
+        cerr << "Error opening file." << endl;
+        return 1;
+    }
+
+    int Value1 = 1234;
+    int intArray[5];
+    savemultiple(intArray);
+    float floatArray[3];
+    savesquare(floatArray);
+
+    file.write((char*)(&Value1), sizeof(int));
+    file.write((char*)(&intArray), sizeof(intArray));
+    file.write((char*)(&floatArray), sizeof(floatArray));
+
+
+    file.close();
+
+    return 0;
+}
