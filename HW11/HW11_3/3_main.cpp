2fd4a2537365aa3c4a83aa78d2d3c8518a88a26d
chominji0716 <145107983+chominji0716@users.noreply.github.com>
2023-11-17 17:48:36 +0900

    Update 3_main.cpp

--- HW11/HW11_3/3_main.cpp
+++ HW11/HW11_3/3_main.cpp
@@ -3,9 +3,10 @@
 #include "3_class.h"
 using namespace std;
 int main() {
-	LoL akali(5, 10, 2, 3, -5, 0.1, 10,"akali.dat");
-	LoL amumu(7, 100, -3.5, 4, 100, 10, 15,"amumu.dat");
-	LoL annie(12, 17, 0.01, 0.2, 1.5, 0.08, 20,"annie.dat");
-	LoL ashe(10, 20, 4.2, -5.3, 2.1, 0.2, 5,"ashe.dat");
+	LoL akali(5, 10, 2, 3, -5, 0.1, 10, "akali.dat");
+	LoL amumu(7, 100, -3.5, 4, 100, 10, 15, "amumu.dat");
+	LoL annie(12, 17, 0.01, 0.2, 1.5, 0.08, 20, "annie.dat");
+	LoL ashe(10, 20, 4.2, -5.3, 2.1, 0.2, 5, "ashe.dat");
+
 	return 10;
 }
