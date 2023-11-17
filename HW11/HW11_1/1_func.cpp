97fc4f0b08092a4248bb2763de693aea803caa16
chominji0716 <145107983+chominji0716@users.noreply.github.com>
2023-11-17 13:17:26 +0900

    Update 1_func.cpp

--- HW11/HW11_1/1_func.cpp
+++ HW11/HW11_1/1_func.cpp
@@ -1,22 +1,23 @@
 #include "1_class.h"
-
 void LoL::f0(string ss) {
 	aa.open(ss, ios::binary | ios::out);
 	aa.write((char*)&N1, sizeof(int));
 	aa.write((char*)&N2, sizeof(int));
 	aa.write((char*)&a, sizeof(float));
 	aa.write((char*)&b, sizeof(float));
+	aa.write((char*)&x0, sizeof(float));
 	aa.write((char*)&dx, sizeof(float));
 	aa.write((char*)&m, sizeof(int));
 }
+
 void LoL::f1() {
 	int* f1a;
 	f1a = new int[N2 - N1 + 1];
 	for (int i = N1; i <= N2; i++) {
 		f1a[i - N1] = i * (i + 1) / 2;
 	}
-	aa.write((char*)f1a, sizeof(int) * (N2 - N1 + 1));
-	delete f1a;
+	aa.write((char*)f1a, sizeof(float) * (N2 - N1 + 1));
+	delete[] f1a;
 }
 void LoL::f2() {
 	float* f2a, x;
@@ -26,6 +27,6 @@ void LoL::f2() {
 		f2a[i] = a * x + b;
 	}
 	aa.write((char*)f2a, sizeof(int) * m);
-	delete f2a;
+	delete[] f2a;
 	aa.close();
 }
