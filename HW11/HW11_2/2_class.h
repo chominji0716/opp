21113d6ef16e0671d74f27689dad6cc04b791f7a
chominji0716 <145107983+chominji0716@users.noreply.github.com>
2023-11-17 17:45:47 +0900

    Update 2_class.h

--- HW11/HW11_2/2_class.h
+++ HW11/HW11_2/2_class.h
@@ -1,12 +1,11 @@
-#pragma once
-#include <fstream>
 #include <iostream>
+#include <fstream>
 #include <cmath>
 using namespace std;
 
 class LoL {
 public:
-    LoL() : N1(0), N2(0), a(0), b(0), x(0), dx(0), m(0), Sum(0), sum(0), result(nullptr) {}
+    LoL() : N1(0), N2(0), a(0), b(0), x(0), dx(0), m(0), sum(nullptr), result(nullptr) {}
 
     LoL(int n1, int n2, float A, float B, float X, float dX, int M) {
         N1 = n1;
@@ -16,19 +15,21 @@ public:
         x = X;
         dx = dX;
         m = M;
-        Sum = f1();
+        sum = new int[n2-n1+1];
+        f1();
         result = new float[M]; // 동적 할당 전에 배열을 초기화
         f2();
     }
 
     ~LoL() {
         delete[] result;
+        delete[] sum;
     }
 
     // 결과를 파일에 쓰는 함수
     void writeToFile(char* filename) {
         ofstream outf(filename, ios::binary | ios::out);
-        if (!outf) {
+        if(!outf) {
             cout << "Unable to open file: " << filename << endl;
         }
         outf.write((char*)&N1, sizeof(int));
@@ -38,17 +39,15 @@ public:
         outf.write((char*)&x, sizeof(float));
         outf.write((char*)&dx, sizeof(float));
         outf.write((char*)&m, sizeof(int));
-        outf.write((char*)&Sum, sizeof(int));
-        outf.write((char*)result, m * sizeof(float));
-
+        outf.write((char*)sum, (N2-N1+1)*sizeof(int));
+        outf.write((char*)result, m*sizeof(float));
+        
     }
 
-    int f1() {
-        sum = 0;
+    void f1() {
         for (int i = N1; i <= N2; i++) {
-            sum += i;
+            sum[i-N1] = i * (i + 1) / 2;
         }
-        return sum;
     }
 
     void f2() {
@@ -60,8 +59,8 @@ public:
     }
 
 private:
-    int N1, N2, m, Sum;
+    int N1, N2, m;
     float a, b, x, dx;
-    int sum;
-    float* result; // 동적으로 할당된 배열을 가리키는 포인터
+    int *sum;
+    float *result; // 동적으로 할당된 배열을 가리키는 포인터
 };
