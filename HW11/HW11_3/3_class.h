8528bd2ee426904f46c84da73098d2c3ba83de68
chominji0716 <145107983+chominji0716@users.noreply.github.com>
2023-11-17 17:48:04 +0900

    Update 3_class.h

--- HW11/HW11_3/3_class.h
+++ HW11/HW11_3/3_class.h
@@ -1,20 +1,26 @@
 #pragma once
 #include <fstream>
 using namespace std;
-
 class LoL {
 public:
-	LoL() {};
+    LoL() {};
     int N1, N2, m;
     float a, b, x0, dx;
     string filename;
     ofstream dd;
-    LoL(int N1, int N2, float a, float b, float x0, float dx, int m, string filename) {
-        f0(); f1(); f2();
+    LoL(int n1, int n2, float A, float B, float X, float dX, int M, string ss) {
+		N1 = n1;
+		N2 = n2;
+		a = A;
+		b = B;
+		x0 = X;
+		dx = dX;
+		m = M;
+		filename = ss;
+		f0(); f1(); f2();
     }
-    ~LoL() {}
-
-	void f0();
+	~LoL() {}
+	void f0(); 
 	void f1();
 	void f2();
 };
