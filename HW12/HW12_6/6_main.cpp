1d6336a9899ba392f0391b821f9ad868729ae5ae
chominji0716 <145107983+chominji0716@users.noreply.github.com>
2023-11-24 02:20:53 +0900

    Update 6_main.cpp

--- HW12/HW12_6/6_main.cpp
+++ HW12/HW12_6/6_main.cpp
@@ -1,1 +1,15 @@
-dd
+#include <iostream>
+using namespace std;
+#include "Car.h"
+int main(int argc, char* argv[]) {
+	Car* A = new Car(5, 2, 50000);
+	Car* B = new Car(10, 4, 100000);
+	Car C = *A + *B;
+	cout << "The speed is " << C.speed << "\n";
+	cout << "The number of wheels "<< C.wheels <<"\n";
+	cout << "The number of wheels " << C.price << "\n";
+
+	delete A,B;
+	getchar();
+	return 123;
+}
