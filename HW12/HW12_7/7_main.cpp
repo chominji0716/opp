5e9e4e6aee47cd6dab496fd532b5e4097d1fce74
chominji0716 <145107983+chominji0716@users.noreply.github.com>
2023-11-24 02:40:29 +0900

    Update 7_main.cpp

--- HW12/HW12_7/7_main.cpp
+++ HW12/HW12_7/7_main.cpp
@@ -1,1 +1,16 @@
-dd
+#include <iostream>
+#include "ani.h"
+#include "hum.h"
+#include "bird.h"
+#include "fish.h"
+using namespace std;
+int main() {
+	animal *a[3];
+	for (int i = 0; i < 2; i++) a[i] = new fish;
+	fish c;
+	a[2] = &c;
+	c.sea();
+	a[2]->sea;
+	((fish*)a[2])->sea();
+	return 123;
+}
